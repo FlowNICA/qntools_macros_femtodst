953842ed28a01e37ae75f5ca5d7a6202dd7ae4fd
ParfenovPeter <terrylapard@gmail.com>
2023-01-13 08:31:58 +0300

    Small change in start_corrections.sh: increased time for a jobs

--- start_corrections.sh
+++ start_corrections.sh
@@ -7,8 +7,8 @@
 #$ -N run_makeQvectors
 # -q all.q
 # -l h=(ncx112|ncx115|ncx117|ncx121|ncx124|ncx12[6-7]|ncx130|ncx132|ncx134|ncx136|ncx138|ncx141|ncx144|ncx150|ncx152|ncx159|ncx16[0-9]|ncx17[0-2]|ncx17[4-6]|ncx18[0-1]|ncx18[4-5]|ncx20[1-3]|ncx20[5-8]|ncx21[1-8]|ncx22[4-8]|ncx23[2-8])
-#$ -l h_rt=01:30:00
-#$ -l s_rt=01:30:00
+#$ -l h_rt=08:30:00
+#$ -l s_rt=08:30:00
 #$ -t 1-100
 #$ -o /dev/null
 #$ -e /dev/null
